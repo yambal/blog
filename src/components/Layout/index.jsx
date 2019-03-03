36a18a3003984d662dc9cea99e478ba6d29b1bcc
June YAMAMOTO <june@yambal.net>
2019-03-03 13:53:24 +0900

    typo

--- src/components/Layout/index.jsx
+++ src/components/Layout/index.jsx
@@ -3,22 +3,21 @@ import Helmet from 'react-helmet'
 import '../../assets/scss/init.scss'
 import styled from "styled-components"
 
-const Layout = styled.div`
+const LayoutStyle = styled.div`
   max-width: 66.875rem;
   margin-left: auto;
   margin-right: auto;
 `
-
 
 class Layout extends React.Component {
   render() {
     const { children } = this.props
 
     return (
-      <Layout>
+      <LayoutStyle>
         <Helmet defaultTitle="Blog by John Doe" />
         {children}
-      </Layout>
+      </LayoutStyle>
     )
   }
 }
